ce3a593461615e58cb3ff6645686795d51cd9146
tenhishadow <adm@tenhi.dev>
2019-12-18 21:45:08 +0300

    basic refactoring

--- mbkp.sh
+++ mbkp.sh
@@ -24,42 +24,46 @@ SC_GROUP="root"		# default group
 
 #######################################################################################################################
 # Importing target config where you can override options
-source $1
+source "${1}"
 #######################################################################################################################
 
 # Functions
 #### Utils #############################################################################################
-CMD_FIND=$(which find)
-CMD_MV=$(which mv)
-CMD_GZ=$(which gzip)
-CMD_CHO=$(which chown)
-CMD_CHM=$(which chmod)
-CMD_MKD=$(which mkdir)" -p "
-CMD_RM=$(which rm)
+CMD_FIND=$(command -v find)
+CMD_MV=$(command -v mv)
+CMD_GZ=$(command -v gzip)
+CMD_CHO=$(command -v chown)
+CMD_CHM=$(command -v chmod)
+CMD_MKD="$(command -v mkdir) -p "
+CMD_RM=$(command -v rm)
 CMD_DATE=$(date +%Y%m%d_%H%M) # date in format YYYYMMDD_HHmm
-CMD_SSL=$(which openssl)
-CMD_SSH=$(which ssh)
-CMD_SCP=$(which scp)
+CMD_SSL=$(command -v openssl)
+CMD_SSH=$(command -v ssh)
+CMD_SCP=$(command -v scp)
 
 ########################################################################################################
-ST_FULL=$ST_ROOT/$ST_HOSTNAME"/"        # full path to .backup (/root_storage/hostname/)
-ST_ARCH=$ST_FULL"archive/"              # full path to archive (/root_storage/hostname/archive)
-TGT_BKPNAME_BIN=$ST_HOSTNAME"_"$CMD_DATE".backup"
-TGT_BKPNAME_EXP=$ST_HOSTNAME"_"$CMD_DATE".export"
+ST_FULL=${ST_ROOT}/${ST_HOSTNAME}"/"        # full path to .backup (/root_storage/hostname/)
+ST_ARCH=${ST_FULL}"archive/"              # full path to archive (/root_storage/hostname/archive)
+TGT_BKPNAME_BIN="${ST_HOSTNAME}_${CMD_DATE}.backup"
+TGT_BKPNAME_EXP="${ST_HOSTNAME}_${CMD_DATE}.export"
 
-SSH_OPT=" -o ConnectionAttempts=5 -o ConnectTimeout=5s \
--o PasswordAuthentication=no -o PreferredAuthentications=publickey \
--o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
--o GlobalKnownHostsFile=/dev/null -o CheckHostIP=no "
-SSH_STR="$CMD_SSH -2 -4 -p $TGT_PORT -l $TGT_USER $TGT_IP $SSH_OPT"
-SCP_STR="$CMD_SCP -2 -4 -B $SSH_OPT -P $TGT_PORT $TGT_USER@$TGT_IP:/$TGT_BKPNAME_BIN $ST_FULL"
+SSH_OPT=" -o ConnectionAttempts=5 \
+  -o ConnectTimeout=5s \
+  -o PasswordAuthentication=no \
+  -o PreferredAuthentications=publickey \
+  -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  -o GlobalKnownHostsFile=/dev/null \
+  -o CheckHostIP=no "
+SSH_STR="${CMD_SSH} -2 -4 -p ${TGT_PORT} -l $TGT_USER ${TGT_IP} ${SSH_OPT}"
+SCP_STR="${CMD_SCP} -2 -4 -B ${SSH_OPT} -P ${TGT_PORT} $TGT_USER@${TGT_IP}:/${TGT_BKPNAME_BIN} ${ST_FULL}"
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #### Defining functions ################################################################################
 
 function fn_check_log {
 # Function for checking need of creating logfile
-LOG=$ST_ROOT/"LOG.txt"                                  # log-file location
+LOG=${ST_ROOT}/"LOG.txt"                                  # log-file location
 
 
  if [[ -r $LOG ]]
@@ -80,7 +84,7 @@ LOG=$ST_ROOT/"LOG.txt"                                  # log-file location
 function fn_check_readme {
 # Function for checking need of creating readme
 
-README=$ST_ROOT"/README.txt"      # README File
+README=${ST_ROOT}"/README.txt"      # README File
 
  if [[ -r $README ]]
   then
@@ -101,51 +105,46 @@ README=$ST_ROOT"/README.txt"      # README File
 function fn_check_directory {
 # Function for checking||creating full-path dirs
 
- if [[ -d $ST_FULL"archive" && -r $ST_FULL"archive" ]]
+ if [[ -d "${ST_FULL}archive" && -r "${ST_FULL}archive" ]]
   then
    return 0
   else
-   $CMD_MKD $ST_FULL"archive"
-   $CMD_CHO $SC_USER:$SC_GROUP $ST_FULL
-   $CMD_CHM 755 $ST_FULL
+   ${CMD_MKD} "${ST_FULL}archive"
+   ${CMD_CHO} ${SC_USER}:${SC_GROUP} "${ST_FULL}"
+   ${CMD_CHM} 755 "${ST_FULL}"
  fi
 }
 
 function fn_mikrotik_cleanup {
 # Function for cleaning up target mikrotik
 
-$SSH_STR "ip dns cache flush"
-$SSH_STR "console clear-history"
-}
-function fn_mikrotik_fixtime {
-# Function for setting ntp client
-
-$SSH_STR "ip cloud set update-time=no; system ntp client set primary-ntp=0.0.0.0 secondary-ntp=0.0.0.0 enabled=yes server-dns-names=pool.ntp.org"
+${SSH_STR} "ip dns cache flush"
+${SSH_STR} "console clear-history"
 }
 
 function fn_backup_binary {
  # Function for saving binary backup
  # Changed 20170901
- T_BKPSTR="system backup save name=$TGT_BKPNAME_BIN dont-encrypt=no password=$BKP_BINPWD"
- T_BKPCLN="file remove [find name=$TGT_BKPNAME_BIN]"
+ T_BKPSTR="system backup save name=${TGT_BKPNAME_BIN} dont-encrypt=no password=${BKP_BINPWD}"
+ T_BKPCLN="file remove [find name=${TGT_BKPNAME_BIN}]"
 
 
  # Put output result exec mikrotik command to /dev/null
- $SSH_STR $T_BKPSTR > /dev/null          # Initializing backup
+ ${SSH_STR} "${T_BKPSTR}" > /dev/null          # Initializing backup
 
- sleep $IDL && $SCP_STR                  # Copy file to storage
- sleep $IDL && $SSH_STR $T_BKPCLN        # Remove created file on mikrotik
+ sleep ${IDL} && ${SCP_STR}                  # Copy file to storage
+ sleep ${IDL} && ${SSH_STR} "${T_BKPCLN}"    # Remove created file on mikrotik
 }
 function fn_backup_export {
  # Function for saving exported config
 
  # NOTE: decrypt the file
  # openssl des3 -d -salt -in encryptedfile.txt -out normalfile.txt
- EXP_TMP_FILE="/tmp/"$RANDOM".export"
+ EXP_TMP_FILE="/tmp/${RANDOM}.export"
 
- sleep $IDL && $SSH_STR export > $EXP_TMP_FILE
- $CMD_SSL des3 -salt -k $BKP_EXPPWD -in $EXP_TMP_FILE -out $ST_FULL$TGT_BKPNAME_EXP".des3"
- $CMD_RM $EXP_TMP_FILE
+ sleep ${IDL} && ${SSH_STR} export > ${EXP_TMP_FILE}
+ ${CMD_SSL} des3 -salt -k ${BKP_EXPPWD} -in ${EXP_TMP_FILE} -out "${ST_FULL}${TGT_BKPNAME_EXP}.des3"
+ ${CMD_RM} ${EXP_TMP_FILE}
 
 }
 
@@ -153,26 +152,26 @@ function fn_backup_retention {
 # Function for rotating old backups
 
  # Search old backups only one directory, not tree
-  $CMD_FIND $ST_FULL -maxdepth 1 -mtime +$ST_RTN -type f -exec $CMD_MV {} $ST_ARCH \;
+  ${CMD_FIND} "${ST_FULL}" -maxdepth 1 -mtime +$ST_RTN -type f -exec "${CMD_MV}" {} "${ST_ARCH}" \;
  # Add into archive only not gz files
-  $CMD_FIND $ST_ARCH -not -name "*.gz" -type f -exec $CMD_GZ {} \;
+  ${CMD_FIND} "${ST_ARCH}" -not -name "*.gz" -type f -exec "${CMD_GZ}" {} \;
 }
 
 function fn_log {
 # Function for recording results to logfile
 
- if [[ -r $ST_FULL$TGT_BKPNAME_BIN ]]
+ if [[ -r ${ST_FULL}${TGT_BKPNAME_BIN} ]]
   then
-   echo $CMD_DATE";okay;"$TGT_BKPNAME_BIN >> $LOG
+   echo "${CMD_DATE};okay;${TGT_BKPNAME_BIN}" >> $LOG
   else
-   echo $CMD_DATE";fail;"$TGT_BKPNAME_BIN >> $LOG
+   echo "${CMD_DATE};fail;${TGT_BKPNAME_BIN}" >> $LOG
  fi
 
- if [[ -r $ST_FULL$TGT_BKPNAME_EXP".des3" ]]
+ if [[ -r ${ST_FULL}${TGT_BKPNAME_EXP}".des3" ]]
   then
-   echo $CMD_DATE";okay;"$TGT_BKPNAME_EXP".des3" >> $LOG
+   echo "${CMD_DATE};okay;${TGT_BKPNAME_EXP}.des3" >> $LOG
   else
-   echo $CMD_DATE";fail;"$TGT_BKPNAME_EXP".des3" >> $LOG
+   echo "${CMD_DATE};fail;${TGT_BKPNAME_EXP}.des3" >> $LOG
  fi
 
 }
@@ -190,8 +189,7 @@ fn_backup_retention                     # Handling old backups
 fn_mikrotik_cleanup                     # Initial cleanup
 [[ $? -ne 0 ]] && fn_log && echo "ERR: cannot establish ssh-connection" && exit 1
 
-sleep $IDL && fn_backup_binary           # save binary backup
-sleep $IDL && fn_backup_export           # save exported config
-sleep $IDL && fn_mikrotik_fixtime
-sleep $IDL && fn_mikrotik_cleanup        # Clean it again to hide commands
-fn_log                                   # Recording backup results to file
+sleep ${IDL} && fn_backup_binary        # save binary backup
+sleep ${IDL} && fn_backup_export        # save exported config
+sleep ${IDL} && fn_mikrotik_cleanup     # Clean it again to hide commands
+fn_log                                  # Recording backup results to file
