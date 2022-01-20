3f3e74bf88c9f967e8781c26710ea00831d1fef2
tenhishadow <adm@tenhi.dev>
2022-01-20 10:32:27 +0100

    fix

--- version-get.sh
+++ version-get.sh
@@ -1,5 +1,30 @@
 function fn_check_ros_version () {
-  awk -F"[:.]" '/version/{gsub(/ /,""); print $2}' "${1}"
+  ROS_VERSION=$( awk -F '[:.]' '/version/{gsub(/ /,""); print $2}' "${1}" )
+
+  echo "...checking ${ROS_VERSION}"
+
+  case ${ROS_VERSION} in
+    "6")
+    echo "version ${ROS_VERSION} is supported"
+    echo "export command will be:"
+    echo "export terse"
+    ;;
+    "7")
+    echo "version ${ROS_VERSION} is supported"
+    echo "export command will be:"
+    echo "export terse show-sensitive"
+    ;;
+    "*")
+    echo "version ${ROS_VERSION} is NOT supported"
+    ;;
+  esac
 }
 
-fn_check_ros_version temp-input
+
+for i in temp-v6 temp-v7 temp-v7x temp-v8 temp-v9; do
+
+  echo "file $i"
+  fn_check_ros_version $i
+  echo ""
+done
+
