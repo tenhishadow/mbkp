74232ce5b6cf2db331dead859a2a2186489e8b67
tenhishadow <adm@tenhi.dev>
2023-03-26 16:16:57 +0200

    fix

--- .test/test.sh
+++ .test/test.sh
@@ -24,3 +24,17 @@ qemu-system-x86_64 \
 ssh -F .test/.ssh_config chr_test \
   "system resource print"
 
+# check that we run the same we launch(ну нет уже веры никому)
+version_running=$(
+ssh -F .test/.ssh_config chr_test \
+  "system resource print" \
+  | awk -F ':' \
+  '/version/ {sub(/^ */, "", $2); print $2}' \
+  | awk '{print $1}' )
+
+echo "DBG: version running $version_running"
+echo "DBG: version expected $1"
+
+[[ $1 != "${version_running}" ]] && \
+  echo "something wrong in test" && \
+  exit 1
