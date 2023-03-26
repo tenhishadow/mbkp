cc2b6dd7d0c6a6fd1c69c990dcc848a794906992
tenhishadow <adm@tenhi.dev>
2023-03-26 13:38:37 +0200

    fix

---
+++ .test/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o nounset
+set -o noclobber
+set -o errexit
+set -o pipefail
+
+[[ -z $1 ]] \
+  && echo "arg1 is not passed" \
+  && exit 1
+
+# deps | get chr disk image
+wget -qO- \
+  "https://download.mikrotik.com/routeros/${1}/chr-${1}.img.zip" \
+  | zcat \
+  > "chr-${1}.img"
+
+# run vm
+qemu-system-x86_64 \
+  -daemonize \
+  -display none \
+  -m 256 \
+  -net nic,model=virtio \
+  -net user,hostfwd=tcp::2345-:22 \
+  -drive file="chr-${1}.img",format=raw,if=ide
+
+# check chr is up and running
+ssh -F .test/.ssh_config chr_test \
+  "system resource print"
+
