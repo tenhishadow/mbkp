aa4bbfac025c691ea00f4589d05452b5ed901399
tenhishadow <adm@tenhi.dev>
2023-03-26 16:38:02 +0200

    fix

--- .test/test.sh
+++ .test/test.sh
@@ -39,6 +39,6 @@ version_running=$(fn_get_running_version)
 echo "DBG: version running $version_running"
 echo "DBG: version expected $1"
 
-[[ "${1}" != "${version_running}" ]] && \
+[[ $1 != "$version_running" ]] && \
   echo "something wrong in test" && \
   exit 1
